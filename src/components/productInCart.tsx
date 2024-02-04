b0ad4f72d6b143fd859d62dd1729cfb3e7f3afb6
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-02-04 09:20:43 +0300

    +

--- src/components/productInCart.tsx
+++ src/components/productInCart.tsx
@@ -1,26 +1,8 @@
-import { useState, useContext, useEffect } from 'react';
+import useProductsCart from '../assets/hooks/useProductsCart';
 import { IProduct } from '../models';
-import { CartContext } from './cartContext';
 
 function ProductCart({ el }: { el: IProduct }) {
- const [quantity, setQuantity] = useState<number>(1);
- const [, setCart] = useContext(CartContext);
-
- const changeQuantity = () => {
-  setQuantity((x) => x + 1);
- };
-
- useEffect(() => {
-  setCart((x: any) =>
-   x.map((elem: IProduct) => {
-    if (elem === el) {
-     elem.quantity = quantity;
-     return elem;
-    }
-    return elem;
-   })
-  );
- }, [quantity]);
+ const { quantity, dispatch } = useProductsCart({ el });
 
  return (
   <li className="flex flex-col items-center last:border-b-0 border-b-[1px] border-[lightgray] p-[10px_5px]">
@@ -31,7 +13,10 @@ function ProductCart({ el }: { el: IProduct }) {
    />
    <p className="text-center mb-[10px]">{el.title}</p>
    <div className="flex items-center">
-    <button className="w-[25px] h-[25px] border rounded-full bg-[url('./img/svg/dash-lg.svg')] bg-cover bg-[black]"></button>
+    <button
+     className="w-[25px] h-[25px] border rounded-full bg-[url('./img/svg/dash-lg.svg')] bg-cover bg-[black]"
+     onClick={() => dispatch('minus')}
+    ></button>
     <input
      className="grow-0 text-[20px] text-center w-[70px] p-0"
      type="text"
@@ -40,7 +25,7 @@ function ProductCart({ el }: { el: IProduct }) {
     />
     <button
      className="w-[25px] h-[25px] border rounded-full bg-[url('./img/svg/plus-lg.svg')] bg-cover bg-[black]"
-     onClick={() => changeQuantity()}
+     onClick={() => dispatch('plus')}
     ></button>
    </div>
   </li>
