d327477eea6cad6d844678b8ab33dc7eeb9f32c1
dmitrytrofim <78739257+dmitrytrofim@users.noreply.github.com>
2024-02-04 15:30:32 +0300

    +

--- src/components/productInCart.tsx
+++ src/components/productInCart.tsx
@@ -1,8 +1,10 @@
-import useProductsCart from '../assets/hooks/useProductsCart';
+import { useDispatch } from 'react-redux';
 import { IProduct } from '../models';
+import { quantityIncrement } from '../store/features/cartSlice';
 
 function ProductInCart({ el }: { el: IProduct }) {
- const { quantity, dispatch } = useProductsCart({ el });
+ // const cart = useSelector((state: any) => state.cart.value);
+ const dispatch = useDispatch();
 
  return (
   <li className="flex flex-col items-center last:border-b-0 border-b-[1px] border-[lightgray] p-[10px_5px]">
@@ -13,19 +15,16 @@ function ProductInCart({ el }: { el: IProduct }) {
    />
    <p className="text-center mb-[10px]">{el.title}</p>
    <div className="flex items-center">
-    <button
-     className="w-[25px] h-[25px] border rounded-full bg-[url('./img/svg/dash-lg.svg')] bg-cover bg-[black]"
-     onClick={() => dispatch('minus')}
-    ></button>
+    <button className="w-[25px] h-[25px] border rounded-full bg-[url('./img/svg/dash-lg.svg')] bg-cover bg-[black]"></button>
     <input
      className="grow-0 text-[20px] text-center w-[70px] p-0"
      type="text"
-     value={quantity}
      readOnly
+     value={el.quantity}
     />
     <button
      className="w-[25px] h-[25px] border rounded-full bg-[url('./img/svg/plus-lg.svg')] bg-cover bg-[black]"
-     onClick={() => dispatch('plus')}
+     onClick={() => dispatch(quantityIncrement(el))}
     ></button>
    </div>
   </li>
--- src/store/features/cartSlice.ts
+++ src/store/features/cartSlice.ts
@@ -1,22 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
- value: [],
-};
-
 export const cartSlice = createSlice({
  name: 'cart',
- initialState,
+ initialState: {
+  value: [],
+ },
  reducers: {
   setCart: (state, { payload }: PayloadAction<any>) => {
    let newProduct = { ...payload };
    newProduct.quantity = 1;
-   state.value = [newProduct, ...state.value];
+   state.value = [newProduct as never, ...state.value];
+  },
+  quantityIncrement: (state: any, { payload }: PayloadAction<any>) => {
+   state.value = [payload, ...state.value];
    console.log(state.value);
   },
  },
 });
 
-export const { setCart } = cartSlice.actions;
+export const { setCart, quantityIncrement } = cartSlice.actions;
 export default cartSlice.reducer;
